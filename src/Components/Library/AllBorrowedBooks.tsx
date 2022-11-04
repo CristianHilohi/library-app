6cc8b64be71b7fe0debee1da5885f61529ad3d67
CRISTIAN_HILOHI <CRISTIAN.HILOHI@myonvent.com>
2022-11-04 20:40:51 +0200

    add all borrowed component

--- src/Components/Library/AllBorrowedBooks.tsx
+++ src/Components/Library/AllBorrowedBooks.tsx
@@ -1,23 +1,25 @@
 import {useAuth0} from "@auth0/auth0-react";
 import {useEffect} from "react";
+import {useUsers} from "../../Hooks/useUsers";
 import {useNavigate} from "react-router-dom";
 import {CircularProgress} from "@mui/material";
 
-export const MyBooks = () => {
-    const {isAuthenticated, isLoading} = useAuth0();
+export const AllBorrowedBooks = () => {
+    const {user, isLoading} = useAuth0();
+    const {userIsAdmin} = useUsers();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if(!isLoading && !isAuthenticated) {
+    useEffect(()=> {
+        if(!userIsAdmin()) {
             navigate('/');
         }
-    }, [isLoading]);
+    }, [user])
 
     if(isLoading) {
         return <div className='page-container'><CircularProgress /></div>
     }
 
-    return <div className='my-books page-container'>
-        <h2>My borrowed books:</h2>
-        </div>
+    return <div>
+        <h2>All borrowed books:</h2>
+    </div>
 }
